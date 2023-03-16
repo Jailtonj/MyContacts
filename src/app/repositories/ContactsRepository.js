2b730dcc0204ce17913f0a26c3440e9e53c570aa
jailtonj <jailtonpereira762@gmail.com>
2023-03-16 18:51:24 -0300

    Refatorando o método delete

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,24 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Jailton',
-    email: 'jailton@gmail.com',
-    phone: '1231233213',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jose',
-    email: 'jose@gmail.com',
-    phone: '1231aaaa233213',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -61,11 +42,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
